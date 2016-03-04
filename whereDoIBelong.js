50efc9a57ad98b037427b236416bd11d62904609
Christine L. Feaster <feasterc@gmail.com>
2016-03-04 11:34:47 -0800

    Add pseudocode for challenge 'Where Do I Belong'

---
+++ whereDoIBelong.js
@@ -0,0 +1,11 @@
+function where(arr, num) {
+    var index;
+    // sort arr
+    // iterate over arr
+    // compare num to item at index
+    // - if num is larger then continue
+    // - if num is smaller, return index - 1
+    return index;
+}
+
+where([40, 60], 50);
