5a331c9bb9994ccbfaf27fd3e0602864e4c307b8
Christine L. Feaster <feasterc@gmail.com>
2016-03-04 12:18:41 -0800

    [WIP] Solve 'Where Do I Belong' for insertion
     - Must implement solution to handle case where num is larger than all items in array

--- whereDoIBelong.js
+++ whereDoIBelong.js
@@ -1,11 +1,28 @@
 function where(arr, num) {
-    var index;
     // sort arr
-    // iterate over arr
-    // compare num to item at index
-    // - if num is larger then continue
-    // - if num is smaller, return index - 1
-    return index;
+    arr = arr.sort(function(a, b) {
+        return a - b;
+    });
+    console.log(arr);
+    // iterate over sorted arr
+    for (var i=0, l=arr.length; i<l; i++) {
+        // - if num is larger, then continue
+        // - if num is smaller, then return index
+        if (isGreater(num, arr[i])) {
+            console.log(i);
+            return i;
+        }
+        // - if num is smaller than last index, return i + 1
+        // [insert code]
+    }
+    function isGreater(control, candidate) {
+        // compare num to item at index
+        return control <= candidate;
+    }
 }
 
-where([40, 60], 50);
+console.log(where([10, 20, 30, 40, 50], 35) == 3);
+console.log(where([10, 20, 30, 40, 50], 30) == 2);
+console.log(where([40, 60, 30], 50) == 2);
+console.log(where([3, 10, 5], 3) == 0);
+console.log(where([2, 5, 10], 15) == 3);
